3ce69bb4500ca77425c4df3845e7917ef50bb2a1
ParadigmZero <josephrcalton@gmail.com>
2021-03-24 09:56:21 +0000

    Simple skeleton of upload done

--- homelearnhut/src/components/Upload/index.js
+++ homelearnhut/src/components/Upload/index.js
@@ -3,3 +3,26 @@ This is opened when the upload button is clicked on the TopBar component
 
 See wireframe for details of this component
 */
+const Upload = () => {
+    return (
+
+        <>
+            Enter title
+            <br />
+            <input></input>
+            Comment:
+            <br />
+            <input></input>
+            <br />
+            Due date:
+            <br />
+            <input></input>
+            <br />
+            <button>Upload</button>
+            <br />
+            <button>Submit</button>
+        </>
+    );
+}
+
+export default Upload;
