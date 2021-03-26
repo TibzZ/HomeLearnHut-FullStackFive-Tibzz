2bde2258b9b25ff09b434ca0a9308e5e3b654807
ParadigmZero <josephrcalton@gmail.com>
2021-03-26 15:16:18 +0000

    slight change

--- homelearnhut/src/libs/initialState.js
+++ homelearnhut/src/libs/initialState.js
@@ -2,5 +2,4 @@ import { homework } from "./homework";
 import * as pages from "./pages";
 
 // Variables
-//export const initialState = { page: pages.FEED, homeworkIndex: 0, childIndex: 0, homework: [...homework] };
-export const initialState = { page: pages.CLASSROOM, homeworkIndex: 0, childIndex: 0, homework: [...homework] };
+export const initialState = { page: pages.FEED, homeworkIndex: 0, childIndex: 0, homework: [...homework] };
