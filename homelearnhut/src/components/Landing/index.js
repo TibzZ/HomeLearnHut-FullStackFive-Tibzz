498bbe2259d19517e6cb0ad48a388501daa986d8
Tibz <thibaut_marechal@yahoo.com>
2021-03-28 21:58:27 +0100

    Sign up font changed

--- homelearnhut/src/components/Landing/index.js
+++ homelearnhut/src/components/Landing/index.js
@@ -9,14 +9,14 @@ import css from "../Landing/Landing.module.css";
 
 const Landing = () => {
     return (
-    <div className={css.Box1}>
+    <div className={css.Box1}> 
     <div className={css.BackgroundLanding}>
-        <h1 className={css.title1}>Welcome</h1>
+        <h1 className={css.Title1}>Welcome</h1>
         {/* For CSS test purpose only: */}
         {/* <h2 className={css.Test}>Css Test</h2> */}
         <p className={css.AppLogo}/>
         <AuthButton />
-        <p>If you haven't got an account, click the Sign up button below</p>
+        <p className={css.Text1}>No account yet? Click the Sign up button! </p>
         <button>Sign Up</button>
     </div>
     </div>
