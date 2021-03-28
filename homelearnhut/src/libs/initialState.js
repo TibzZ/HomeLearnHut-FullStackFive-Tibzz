eddae00cef675a14e695fffbcb7e20d1beb18f61
ParadigmZero <josephrcalton@gmail.com>
2021-03-28 20:18:08 +0100

    Adding various things to allow the dev to function better

--- homelearnhut/src/libs/initialState.js
+++ homelearnhut/src/libs/initialState.js
@@ -2,4 +2,4 @@ import { homework } from "./homework";
 import * as pages from "./pages";
 
 // Variables
-export const initialState = { page: pages.FEED, homeworkIndex: 0, childIndex: 0, homework: [...homework] };
+export const initialState = { page: pages.LOADING, homeworkIndex: 0, childIndex: 0, homework: [...homework] };
