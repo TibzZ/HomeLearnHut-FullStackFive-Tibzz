8afe4c446ca12f63ab9ee4423ace217d1c9204ee
Tibz <thibaut_marechal@yahoo.com>
2021-03-30 10:44:22 +0100

    grid start

--- homelearnhut/src/components/Landing/index.js
+++ homelearnhut/src/components/Landing/index.js
@@ -9,16 +9,29 @@ import css from "../Landing/Landing.module.css";
 
 const Landing = () => {
     return (
-    <div className={css.Box1}> 
+    <div className={css.container}>
+
+        {/* <div className="item">1
+        
+        </div>
+        
+        <div className="item">2</div>
+           
+        <div className="item">3</div>
+           
+        <div className="item">4</div>
+         
+        <div className="item">5</div> */}
+
+
     <div className={css.BackgroundLanding}>
         <h1 className={css.Title1}>Welcome</h1>
-        {/* For CSS test purpose only: */}
-        {/* <h2 className={css.Test}>Css Test</h2> */}
         <p className={css.AppLogo}/>
         <AuthButton />
         <p className={css.Text1}>No account yet? Click the Sign up button! </p>
         <button>Sign Up</button>
     </div>
+
     </div>
     );
 }
