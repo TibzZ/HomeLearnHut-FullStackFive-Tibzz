6dcf8891ff458b452b4eb13b851ee397d6552a6c
Tibz <thibaut_marechal@yahoo.com>
2021-03-31 15:52:36 +0100

    main list of names database WIP

--- src/libs/children.js
+++ src/libs/children.js
@@ -1,37 +1,117 @@
 // 18 images currently on S3
 
 
-
+//Amelia, Ariella, Arthur, Elliott
 
 export const children = [
   {
-    name: "Cassian",
+    name: "Olivia",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/1.png",
     homeworkMarked: false,
     individualHomeworkImage: null, comment: null
   },
   {
-    name: "Maya",
+    name: "Noah",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/2.png",
     homeworkMarked: false,
     individualHomeworkImage: null, comment: null
   },
   {
-    name: "Elsie",
+    name: "",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/3.png",
     homeworkMarked: false,
     individualHomeworkImage: null, comment: null
   },
   {
-    name: "Raff",
+    name: "",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/4.png",
     homeworkMarked: false,
     individualHomeworkImage: null, comment: null
   },
   {
-    name: "Fifi",
+    name: "Lily",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/5.png",
     homeworkMarked: false,
     individualHomeworkImage: null, comment: null
   },
+  {
+    name: "Charlie",
+    avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/6.png",
+    homeworkMarked: false,
+    individualHomeworkImage: null, comment: null
+  },
+  {
+    name: "Ivy",
+    avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/7.png",
+    homeworkMarked: false,
+    individualHomeworkImage: null, comment: null
+  },
+  {
+    name: "",
+    avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/8.png",
+    homeworkMarked: false,
+    individualHomeworkImage: null, comment: null
+  },
+  {
+    name: "Edith",
+    avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/9.png",
+    homeworkMarked: false,
+    individualHomeworkImage: null, comment: null
+  },
+  {
+    name: "Jake",
+    avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/10.png",
+    homeworkMarked: false,
+    individualHomeworkImage: null, comment: null
+  },
+  {
+    name: "Elliott",
+    avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/11.png",
+    homeworkMarked: false,
+    individualHomeworkImage: null, comment: null
+  },
+  {
+    name: "Grace",
+    avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/12.png",
+    homeworkMarked: false,
+    individualHomeworkImage: null, comment: null
+  },
+  {
+    name: "Liam",
+    avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/13.png",
+    homeworkMarked: false,
+    individualHomeworkImage: null, comment: null
+  },
+  {
+    name: "Y",
+    avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/14.png",
+    homeworkMarked: false,
+    individualHomeworkImage: null, comment: null
+  },
+  {
+    name: "Fifi",
+    avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/15.png",
+    homeworkMarked: false,
+    individualHomeworkImage: null, comment: null
+  },
+  {
+    name: "Fifi",
+    avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/16.png",
+    homeworkMarked: false,
+    individualHomeworkImage: null, comment: null
+  },
+  {
+    name: "Fifi",
+    avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/17.png",
+    homeworkMarked: false,
+    individualHomeworkImage: null, comment: null
+  },
+  {
+    name: "Fifi",
+    avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/18.png",
+    homeworkMarked: false,
+    individualHomeworkImage: null, comment: null
+  },
 ];
+
+
