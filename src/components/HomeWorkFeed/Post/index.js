06d044deb70dc2e78ad0c6a44f43e8c6978f8153
Jenn <jenndoyle@hotmail.co.uk>
2021-03-31 08:30:09 +0100

    feed

--- src/components/HomeWorkFeed/Post/index.js
+++ src/components/HomeWorkFeed/Post/index.js
@@ -3,29 +3,26 @@ import css from "./Post.module.css";
 const Post = ({ homework, index, clickToClassroom }) => {
   return (
     <div className={css.container}>
-    <div className={css.image}>
-      <button onClick={(index) => clickToClassroom(index)}>
-        <img src={homework.image} alt="homework" />
-      </button>
-      </div>
-      <div className={css.textbox} >
+       <div className={css.textbox}>
         {homework.name}
+        <br />
+        date set:  {homework.dateSet}
         <br />
         {homework.imageUrl}
         <br />
-        date set
+        </div>
+      <div className={css.image}>
+        <button onClick={(index) => clickToClassroom(index)}>
+          <img src={homework.image} alt="homework" />
+        </button>
+      </div>
+        <div className={css.textbox2}>
+        Comment:
+        {homework.comment}
         <br />
-        {homework.dateSet}
+        date due: {homework.dateDue}
         <br />
-        date due
-        <br />
-        {homework.dateDue}
-        <br />
-      <br />
-      Comment:
-      {homework.comment}
-      <br />
-    </div>
+      </div>
     </div>
   );
 };
