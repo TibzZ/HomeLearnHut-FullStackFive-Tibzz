461193a407b4a9022e7b928187b24cbb0fbe04ee
ParadigmZero <josephrcalton@gmail.com>
2021-03-31 14:41:07 +0100

    Formatting of HomeworkViewer was canvas is OK now

--- src/components/MyClassroom/Student/index.js
+++ src/components/MyClassroom/Student/index.js
@@ -1,18 +1,17 @@
 //import css from "../Student/Student.module.css";
 import React from "react";
 import css from "./Student.module.css";
-import homework from "../../HomeworkViewer/CanvasTool/homework.png";
 import { TiInputChecked } from "react-icons/ti";
 
 
 function Student({ handleClick, name, avatar }) {
- 
+
   return (
 
-    
+
     <>
-      <button className={css.profilebtn} style={{ backgroundImage:`url(${avatar})` }} onClick={handleClick}>
-      {console.log(avatar)}
+      <button className={css.profilebtn} style={{ backgroundImage: `url(${avatar})` }} onClick={handleClick}>
+        {console.log(avatar)}
       </button>
       <p>Name:{name} </p>
       <br />
