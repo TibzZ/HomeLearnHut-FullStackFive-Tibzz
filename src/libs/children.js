999be000ff423b1c592e008fde31cbbcafc2746f
ParadigmZero <josephrcalton@gmail.com>
2021-04-01 12:21:45 +0100

    geting the scribble info is easy, but loading up a scribble is harder, it pops up but then dissappears straight away

--- src/libs/children.js
+++ src/libs/children.js
@@ -8,109 +8,109 @@ export const children = [
     name: "Amelia",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/1.png",
     homeworkMarked: false,
-    individualHomeworkImage: null, comment: null
+    individualHomeworkImage: null, annotation: null, comment: null
   },
   {
     name: "Ariella",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/2.png",
     homeworkMarked: false,
-    individualHomeworkImage: null, comment: null
+    individualHomeworkImage: null, annotation: null, comment: null
   },
   {
     name: "Arthur",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/3.png",
     homeworkMarked: false,
-    individualHomeworkImage: null, comment: null
+    individualHomeworkImage: null, annotation: null, comment: null
   },
   {
     name: "Charlie",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/4.png",
     homeworkMarked: false,
-    individualHomeworkImage: null, comment: null
+    individualHomeworkImage: null, annotation: null, comment: null
   },
   {
     name: "Elliott",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/5.png",
     homeworkMarked: false,
-    individualHomeworkImage: null, comment: null
+    individualHomeworkImage: null, annotation: null, comment: null
   },
   {
     name: "Emma",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/6.png",
     homeworkMarked: false,
-    individualHomeworkImage: null, comment: null
+    individualHomeworkImage: null, annotation: null, comment: null
   },
   {
     name: "Grace",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/7.png",
     homeworkMarked: false,
-    individualHomeworkImage: null, comment: null
+    individualHomeworkImage: null, annotation: null, comment: null
   },
   {
     name: "Isaac",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/8.png",
     homeworkMarked: false,
-    individualHomeworkImage: null, comment: null
+    individualHomeworkImage: null, annotation: null, comment: null
   },
   {
     name: "Ivy",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/9.png",
     homeworkMarked: false,
-    individualHomeworkImage: null, comment: null
+    individualHomeworkImage: null, annotation: null, comment: null
   },
   {
     name: "Jake",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/10.png",
     homeworkMarked: false,
-    individualHomeworkImage: null, comment: null
+    individualHomeworkImage: null, annotation: null, comment: null
   },
   {
     name: "Liam",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/11.png",
     homeworkMarked: false,
-    individualHomeworkImage: null, comment: null
+    individualHomeworkImage: null, annotation: null, comment: null
   },
   {
     name: "Lily",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/12.png",
     homeworkMarked: false,
-    individualHomeworkImage: null, comment: null
+    individualHomeworkImage: null, annotation: null, comment: null
   },
   {
     name: "Madelyn",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/13.png",
     homeworkMarked: false,
-    individualHomeworkImage: null, comment: null
+    individualHomeworkImage: null, annotation: null, comment: null
   },
   {
     name: "Matthew",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/17.png",
     homeworkMarked: false,
-    individualHomeworkImage: null, comment: null
+    individualHomeworkImage: null, annotation: null, comment: null
   },
   {
     name: "Noah",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/15.png",
     homeworkMarked: false,
-    individualHomeworkImage: null, comment: null
+    individualHomeworkImage: null, annotation: null, comment: null
   },
   {
     name: "Olivia",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/16.png",
     homeworkMarked: false,
-    individualHomeworkImage: null, comment: null
+    individualHomeworkImage: null, annotation: null, comment: null
   },
   {
     name: "Peppa",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/14.png",
     homeworkMarked: false,
-    individualHomeworkImage: null, comment: null
+    individualHomeworkImage: null, annotation: null, comment: null
   },
   {
     name: "Wyatt",
     avatar: "https://homelearnhut.s3.eu-west-2.amazonaws.com/avatars/18.png",
     homeworkMarked: false,
-    individualHomeworkImage: null, comment: null
+    individualHomeworkImage: null, annotation: null, comment: null
   },
 ];
 
