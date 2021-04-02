a207eb26c4a5ea7f0a4c6e9b9c4f5e68261b9462
Tibz <thibaut_marechal@yahoo.com>
2021-04-02 14:36:15 +0100

    testing start in Post2
    Co-authored-by: Ilker79 <24883379+ilker79@users.noreply.github.com>

--- src/components/HomeWorkFeed/Post/index.test.js
+++ src/components/HomeWorkFeed/Post/index.test.js
@@ -6,8 +6,13 @@ const {
   
   
   test(`This function test the filters are working`, () => {
+    // We need to define filter1 as f1, filter2 as f2
+    // Arrange
+    // Act
+    // Assert
+    
     const actual = Navload({James});
-    const expected = "Greetings, James";
+    const expected = [f1, f2];
   
     expect(actual).toBe(expected);
   });
