1e27a22299aab646514ddf243cdfbdb199341deb
ParadigmZero <josephrcalton@gmail.com>
2021-04-02 10:51:04 +0100

    Filter fixed

--- src/components/HomeWorkFeed/index.js
+++ src/components/HomeWorkFeed/index.js
@@ -9,36 +9,52 @@ A piece of homework in this list can be clicked to lead to the "MyClassroom" pag
 import Post from "./Post";
 import css from "./NewsFeed.module.css";
 import DropDownTerm from "../NavFilter";
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const HomeWorkFeed = ({ homeworkList, clickToClassroom }) => {
-  const [hwkState, setHwkState] = useState(homeworkList)
-  function handleClick(selection){
-    setHwkState(selection);
+
+  // show all
+
+
+  const [filter1, setFilter1] = useState("");
+  const [filter2, setFilter2] = useState("");
+
+
+  function changeFilter(f1, f2) {
+    setFilter1(f1);
+    setFilter2(f2);
   }
-  function showAllHwks(){
-    setHwkState(homeworkList);
+
+
+  function showAllHwks() {
+    setFilter1("");
+    setFilter1("");
   }
 
   return (
     <div>
-      <DropDownTerm hwkState={hwkState} handleClick={handleClick}/>
+      <DropDownTerm handleClick={changeFilter} />
       <button className={css.resetBtn} onClick={showAllHwks}>Show all</button>
       <ul className={css.post}>
 
-        {console.log(homeworkList)}
-        {console.log(hwkState)}
-
         {/* For CSS test purpose only: */}
         {/* <h2 className={css.Test}>Css Test ||</h2> */}
-        {hwkState.map((homework, index) => [
+        {homeworkList.map((homework, index) => [
           <li>
-            <Post 
-              key={index}
-              homework={homework}
-              index={index}
-              clickToClassroom={() => clickToClassroom(index)}
-            />
+
+
+            {(homework.dateSet.includes(filter1) || homework.dateSet.includes(filter2)) ?
+              <Post
+                key={index}
+                homework={homework}
+                index={index}
+                clickToClassroom={() => clickToClassroom(index)}
+              />
+              :
+              null
+            }
+
+
           </li>,
         ]).reverse()}
       </ul>
