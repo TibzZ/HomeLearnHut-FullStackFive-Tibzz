4312a5beb32fa4ffb38fa1d37a2ae27ebf7bb2bf
Tibz <thibaut_marechal@yahoo.com>
2021-04-02 16:06:51 +0100

    Revert "disable dummy test in App folder"
    This reverts commit 97103379d66ce51fe0ac09760d5697a1ae4358c0.

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,16 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import App from './App.js';
+import App from './App';
 
-// jest.config.js
-// Or async function
-module.exports = async () => {
-  return {
-    verbose: true,
-  };
-};
-
-
-test.skip('renders learn react link', () => {
+test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
