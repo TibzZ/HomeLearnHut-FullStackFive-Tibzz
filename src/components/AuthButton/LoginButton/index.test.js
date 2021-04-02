ebfd9be86a151dd98b59b33d2383f562ce99a02e
Tibz <thibaut_marechal@yahoo.com>
2021-04-02 15:38:38 +0100

    testing start in AutButton, login
    Co-authored-by: Ilker79 <24883379+ilker79@users.noreply.github.com>

---
+++ src/components/AuthButton/LoginButton/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LoginButton from './index';
+import {render} from '@testing-library/react';
+import "jest-dom/extent-expect";
+
+it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<LoginButton></LoginButton>, div)
+})
+
