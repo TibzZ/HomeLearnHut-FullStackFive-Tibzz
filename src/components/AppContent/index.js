f23dad1a98a3d45cea3c17196bbfdd41bbe1959e
Jenn <jenndoyle@hotmail.co.uk>
2021-04-13 14:54:04 +0100

    scroll top

--- src/components/AppContent/index.js
+++ src/components/AppContent/index.js
@@ -5,28 +5,31 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "../Header";
 import DropDown from "../DropDown";
 import Footer from "../Footer";
+import ScrollToTop from "../../scrollTop";
 
 function AppContent() {
   return (
     <>
       <Router>
-        <Switch>
-          <Route path="/homeworkViewer">
-            <Header />
-            <DropDown />
-            <HomeworkViewer />
-          </Route>
-          <Route path="/myClassroom">
-            <Header />
-            <DropDown />
-            <MyClassroom />
-          </Route>
-          <Route path="/">
-            <Header />
-            <DropDown />
-            <HomeWorkFeed />
-          </Route>
-        </Switch>
+        <ScrollToTop>
+          <Switch>
+            <Route path="/homeworkViewer">
+              <Header />
+              <DropDown />
+              <HomeworkViewer />
+            </Route>
+            <Route path="/myClassroom">
+              <Header />
+              <DropDown />
+              <MyClassroom />
+            </Route>
+            <Route path="/">
+              <Header />
+              <DropDown />
+              <HomeWorkFeed />
+            </Route>
+          </Switch>
+        </ScrollToTop>
       </Router>
       <Footer />
     </>
